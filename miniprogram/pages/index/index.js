789b30e7057f08d74777eafae2bf6068157de9af
wangguan <943126100@qq.com>
2019-05-09 21:41:45 +0800

    ss

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -96,61 +96,50 @@ Page({
     // })
   },
   onLoad: function () {
-    wx.cloud.callFunction({
-      // 云函数名称
-      name: 'add',
-      // 传给云函数的参数
-      data: {
-        a: 1,
-        b: 2,
-      },
-    })
-      .then(res => {
-        console.log('2122'+JSON.stringify(res.result)) // 3
-      })
-      .catch(console.error)
-    wx.cloud.callFunction({
-      // 云函数名称
-      name: 'login',
-      // 传给云函数的参数
-    })
-      .then(res => {
-        console.log('2122' + JSON.stringify(res)) // 3
-      })
-      .catch(console.error)
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+    // wx.cloud.callFunction({
+    //   // 云函数名称
+    //   name: 'add',
+    //   // 传给云函数的参数
+    //   data: {
+    //     a: 1,
+    //     b: 2,
+    //   },
+    // })
+    //   .then(res => {
+    //     console.log('2122'+JSON.stringify(res.result)) // 3
+    //   })
+    //   .catch(console.error)
+    // wx.cloud.callFunction({
+    //   // 云函数名称
+    //   name: 'login',
+    //   // 传给云函数的参数
+    // })
+    //   .then(res => {
+    //     console.log('2122' + JSON.stringify(res)) // 3
+    //   })
+    //   .catch(console.error)
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+              var str = JSON.stringify(res.userInfo);
+              app.globalData.userInfo = res.userInfo
+            }
           })
+        }else {
+          console.log('没有授权')
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      }
     })
+  },
+  bindGetUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo;
+    var str = JSON.stringify(e.detail.userInfo)
+    app.globalData.userInfo = e.detail.userInfo
   }
 })
