f6d55b048bdaa3a96a60663695ecb7a9315a5aca
王冠 <943126100@qq.com>
2019-05-10 18:04:42 +0800

    11

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,24 +3,24 @@
 const app = getApp()
 Page({
   data: {
-    cur: 2,
-    curIndex: 2, //当前的索引
-    projectList: ['我的','学习呀','吐槽圈'],
+    cur: 1,
+    curIndex: 1, //当前的索引
+    projectList: ['我的', '学习呀', '吐槽圈'],
     projuectIndex: 1,
     background: [
-      'demo-text-1', 
-      'demo-text-2', 
+      'demo-text-1',
+      'demo-text-2',
       'demo-text-3'
     ],
     navImage: [
-      '../../img/1.jpg', 
-      '../../img/2.jpg', 
-      '../../img/3.jpg', 
-      '../../img/4.jpg', 
-      '../../img/5.jpg', 
+      '../../img/1.jpg',
+      '../../img/2.jpg',
+      '../../img/3.jpg',
+      '../../img/4.jpg',
+      '../../img/5.jpg',
       '../../img/6.jpg',
     ],
-    subject:[
+    subject: [
       {
         img: '../../img/ty.png',
         words: '体育'
@@ -45,7 +45,7 @@ Page({
         img: '../../img/ls.png',
         words: '历史'
       }
-      
+
     ],
     learnList: [
       {
@@ -68,7 +68,43 @@ Page({
         img: '../../img/p6.png',
         words: '政治提升'
       },
+    ],
+    tucaoList: [
+      {
+        img: '../../img/tx.jpg',
+        message: '一点要好好学习历史，搞不定那天就穿越了',
+        time: '2019-2-17',
+        name: '金鱼哥'
+      },
+      {
+        img: '../../img/p7.png',
+        message: '我们学习中虽然苦，有许许多多的挫折，困难，等待着我们，所以我们要勇敢的面对困难，挑战困难，永不言败，那么成功离我们就不愿了，成功是要付出努力的，付出汗水，没有能随随便便成功的，所以我们应该付出不懈努力去学习。',
+        time: '2019-2-18',
+        name: '缘分天空'
+      },
+      {
+        img: '../../img/p10.png',
+        message: '我从小就怕黑，小时候学习不好就是因为不敢看黑板。',
+        time: '2019-2-18',
+        name: '大哥别杀我'
+      },
+      {
+        img: '../../img/p12.png',
+        message: '好好学习，天天处对象。',
+        time: '2019-2-18',
+        name: '小苗苗'
+      },
     ]
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+    }
+    return {
+      title: "来自泡面的考研学习推荐",
+      path: "pages/index/index",
+      imageUrl: '../../img/url.png'
+    }
   },
   handleChange({ detail }) {
     this.setData({
@@ -90,7 +126,7 @@ Page({
     });
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     // wx.navigateTo({
     //   url: '../logs/logs'
     // })
@@ -118,7 +154,6 @@ Page({
     //     console.log('2122' + JSON.stringify(res)) // 3
     //   })
     //   .catch(console.error)
-    // 查看是否授权
     wx.getSetting({
       success(res) {
         if (res.authSetting['scope.userInfo']) {
@@ -130,7 +165,7 @@ Page({
               app.globalData.userInfo = res.userInfo
             }
           })
-        }else {
+        } else {
           console.log('没有授权')
         }
       }
@@ -141,5 +176,9 @@ Page({
     app.globalData.userInfo = e.detail.userInfo;
     var str = JSON.stringify(e.detail.userInfo)
     app.globalData.userInfo = e.detail.userInfo
+  },
+  handleContact(e) {
+    console.log(e.path)
+    console.log(e.query)
   }
 })
